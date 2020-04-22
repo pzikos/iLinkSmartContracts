81fe32e87cc1689c00a427a6f4eddd1ec25b08cb
pzikos <63677724+pzikos@users.noreply.github.com>
2020-04-22 21:22:40 +0300

    Update and rename arraysLibrary.sol to ArraysLibrary.sol

--- UdemyCourseSamples/ArraysLibrary.sol
+++ UdemyCourseSamples/ArraysLibrary.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.6.3;
 
-library arraysLibrary { //just like a contract but with no state variables, many openzeppelin libs out there
+library ArraysLibrary { //just like a contract but with no state variables, many openzeppelin libs out there
     
     function searchIndexOfVal(uint[] storage theArray, uint whatToFind) public view returns (uint){//no state, so only view
         for(uint i=0; i<theArray.length; i++){
